968e9945e60a253d1723058a57000360d6528d09
cpu500m <paul108203@naver.com>
2023-12-21 18:46:37 +0900

    requestBody 파싱까지 완료!

--- src/main/java/server/handler/BodyHandler.java
+++ src/main/java/server/handler/BodyHandler.java
@@ -3,11 +3,14 @@ package server.handler;
 import com.sun.net.httpserver.HttpExchange;
 import server.code.Status;
 import server.dto.Info;
+import server.exception.CannotParseParamException;
 import server.exception.UnSupportedMethodException;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URLDecoder;
+import java.util.Map;
 
 public class BodyHandler extends JsonHttpHandler{
     @Override
@@ -21,12 +24,35 @@ public class BodyHandler extends JsonHttpHandler{
         InputStreamReader reader = new InputStreamReader(is);
         StringBuilder sb = new StringBuilder();
 
+        // 첫 공백 버려줌
+        reader.read();
+
         for (int c = reader.read(); c != -1; c = reader.read()) {
             sb.append((char)c);
         }
-        String body = sb.toString();
-        System.out.println("body = " + body);
 
-        return null;
+        String body = sb.toString();
+
+        try {
+            String[] pairs = body.split("\\{|&|}|,");
+            Info info = new Info();
+            Map<String, String> data = info.getData();
+
+            // 파라미터 쪼개서 넣음.
+            for (String pair : pairs) {
+                int idx = pair.indexOf(":");
+                String key = pair.substring(0, idx);
+                key =key.replaceAll("[^a-zA-Z0-9]","");
+                String value = pair.substring(idx+1);
+                value = value.replaceAll("[^a-zA-Z0-9]","");
+
+
+                data.put(URLDecoder.decode(key, "UTF-8"), URLDecoder.decode(value, "UTF-8"));
+            }
+            info.setCount(Long.valueOf(data.size()));
+            return info;
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new CannotParseParamException(e);
+        }
     }
 }
